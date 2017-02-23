b4d8aedd16e997e708fb5fa05cb97fb97f46711a
Miguel Ángel Domínguez Coloma <miguel.dominguez@betssongroup.com>
2017-02-23 10:13:41 +0100

    Improve tests for LIST.

--- test/modules/operations/list.js
+++ test/modules/operations/list.js
@@ -17,27 +17,90 @@ describe(file, function () {
   }
 
   describe('using .list()', function () {
-    it('it should return parsed entries no commented without filters', function () {
-      const tests = [
-        {
-          content: `
-                    0.0.0.0 localhost\n
-                    0.0.0.0 local
-                `,
-          result: [
-            { ip: '0.0.0.0', host: 'localhost', comment: '' },
-            { ip: '0.0.0.0', host: 'local', comment: '' }
-          ]
-        }
-      ]
+    it('should return parsed entries without filters', function () {
+      const content = `
+        0.0.0.0 localhost\n
+        0.0.0.0 local\n
+        1.2.3.4 another#host\n
+        5.6.7.8 #invalid.host\n
+        #1.1.1.1 invalid.me\n
+        abc.def.ghi.jkl invalid.local\n
+        # commented line\n
+        #commented line\n
+        \n
+        #1.1.1.1 hostify\n
+        255.255.255.255 broadcast #broadcast`
 
-      tests.forEach(testCase => {
-        const osFileResolver = osFileResolverMock(testCase.content)
-        const list = target({osFileResolver})
+      const result = [
+        { ip: '0.0.0.0', host: 'localhost', comment: '' },
+        { ip: '0.0.0.0', host: 'local', comment: '' },
+        { ip: '1.2.3.4', host: 'another', comment: '#host' },
+        { ip: '255.255.255.255', host: 'broadcast', comment: '#broadcast' }]
 
-        const entries = list()
-        expect(entries).to.eql(testCase.result)
-      })
+      const osFileResolver = osFileResolverMock(content)
+      const list = target({osFileResolver})
+
+      const entries = list()
+      expect(entries).to.eql(result)
+    })
+
+    it('should return parsed entries with IP filters', function () {
+      const content = `
+        0.0.0.0 one.local\n
+        0.0.0.0 two.local\n
+        0.0.1.0 three.local\n
+        0.1.0.0 four.local\n
+      `
+
+      const osFileResolver = osFileResolverMock(content)
+      const list = target({osFileResolver})
+
+      const filterIpFn = function (ip) { return ip.startsWith('0.0') }
+      const entries = list({ filterIpFn })
+      expect(entries).to.eql([
+        { ip: '0.0.0.0', host: 'one.local', comment: '' },
+        { ip: '0.0.0.0', host: 'two.local', comment: '' },
+        { ip: '0.0.1.0', host: 'three.local', comment: '' }]
+      )
+    })
+
+    it('should return parsed entries with Hosts filters', function () {
+      const content = `
+        0.0.0.0 one.local\n
+        0.0.0.0 two.local\n
+        0.0.1.0 three.local\n
+        0.1.0.0 four.local\n
+      `
+
+      const osFileResolver = osFileResolverMock(content)
+      const list = target({osFileResolver})
+
+      const filterHostFn = function (host) { return host.endsWith('e.local') }
+      const entries = list({ filterHostFn })
+      expect(entries).to.eql([
+        { ip: '0.0.0.0', host: 'one.local', comment: '' },
+        { ip: '0.0.1.0', host: 'three.local', comment: '' }]
+      )
+    })
+
+    it('should return parsed entries with IP and Hosts filters', function () {
+      const content = `
+        0.0.0.0 one.local\n
+        0.0.0.0 two.local\n
+        0.0.1.0 three.local\n
+        0.1.0.0 four.local\n
+      `
+
+      const osFileResolver = osFileResolverMock(content)
+      const list = target({osFileResolver})
+
+      const filterIpFn = function (ip) { return ip.endsWith('0.0') }
+      const filterHostFn = function (host) { return host.endsWith('e.local') }
+
+      const entries = list({ filterIpFn, filterHostFn })
+      expect(entries).to.eql([
+        { ip: '0.0.0.0', host: 'one.local', comment: '' }]
+      )
     })
   })
 })
